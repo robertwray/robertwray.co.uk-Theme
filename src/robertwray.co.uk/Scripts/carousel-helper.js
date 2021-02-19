6abcfc400ddc3a7a0ce63e3357503ad12cfc451a
Robert Wray <2657405+robertwray@users.noreply.github.com>
2021-02-19 21:58:02 +0000

    Updated tweaks for BS4

--- src/robertwray.co.uk/Scripts/carousel-helper.js
+++ src/robertwray.co.uk/Scripts/carousel-helper.js
@@ -11,16 +11,16 @@
 function generateMarkupForCarousel(carouselId, carouselEntries) {
     const markupForSlidesAndIndicators = generateMarkupForIndicatorsAndSlides(carouselId, carouselEntries);
     const finalCarousel = [];
-    finalCarousel.push('<div class="carousel slide" id="' + carouselId + '" data-ride="carousel">');
+    finalCarousel.push('<div id="' + carouselId + '" class="carousel slide" data-ride="carousel">');
     finalCarousel.push(markupForSlidesAndIndicators);
     // Controls: Previous
-    finalCarousel.push('<a class="left carousel-control" role="button" href="#' + carouselId + '" data-slide="prev">');
-    finalCarousel.push('<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>');
+    finalCarousel.push('<a class="carousel-control-prev" role="button" href="#' + carouselId + '" data-slide="prev">');
+    finalCarousel.push('<span class="carousel-control-prev-icon" aria-hidden="true"></span>');
     finalCarousel.push('<span class="sr-only">Previous</span>');
     finalCarousel.push('</a>');
     // Controls: Next
-    finalCarousel.push('<a class="right carousel-control" role="button" href="#' + carouselId + '" data-slide="next">');
-    finalCarousel.push('<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>');
+    finalCarousel.push('<a class="carousel-control-next" role="button" href="#' + carouselId + '" data-slide="next">');
+    finalCarousel.push('<span class="carousel-control-next-icon" aria-hidden="true"></span>');
     finalCarousel.push('<span class="sr-only">Next</span>');
     finalCarousel.push('</a>');
     finalCarousel.push('</div>'); // class="carousel slide"
@@ -33,18 +33,18 @@ function generateMarkupForIndicatorsAndSlides(carouselId, carouselEntries) {
     const slides = [];
     // indicators / slides pre-amble
     indicators.push('<ol class="carousel-indicators">');
-    slides.push('<div class="carousel-inner" role="listbox">');
+    slides.push('<div class="carousel-inner">');
     let counter = 0;
     for (const entry of carouselEntries) {
         const indicatorCssClass = counter == 0 ? 'class="active" ' : '';
-        const slideItemCssClass = counter == 0 ? 'class="item active"' : 'class="item"';
+        const slideItemCssClass = counter == 0 ? 'class="carousel-item active"' : 'class="carousel-item"';
         const titleStyle = entry.inverse ? ' style="color: white;"' : '';
-        indicators.push('<li ' + indicatorCssClass + 'data-target="#' + carouselId + '" data-slide-to="' + counter + '"></li>');
+        indicators.push('<li data-target="#' + carouselId + '" data-slide-to="' + counter + '" ' + indicatorCssClass + '></li>');
         slides.push('<div ' + slideItemCssClass + '>');
         slides.push('<img alt="' + entry.title + '" src="' + entry.imageSrc + '" />');
-        slides.push('<div class="carousel-caption">');
-        slides.push('<h3><a href="' + entry.postSrc + '">' + entry.title + '</a></h3>');
-        slides.push('<div' + titleStyle + '>' + entry.description + '</div>');
+        slides.push('<div class="carousel-caption d-none d-md-block">');
+        slides.push('<h3><a style="color: black; font-weight: 900;" href="' + entry.postSrc + '">' + entry.title + '</a></h3>');
+        slides.push('<p' + titleStyle + '>' + entry.description + '</p>');
         slides.push('</div>'); // class="carousel-caption"
         slides.push('</div>'); // class="item"
         counter++;
