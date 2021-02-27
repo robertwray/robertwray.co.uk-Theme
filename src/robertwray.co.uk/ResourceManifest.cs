43797cbc61137c75126401f98f29516c569f5525
Robert Wray <2657405+robertwray@users.noreply.github.com>
2021-02-27 23:49:54 +0000

    Merge in PJS.Bootstrap

--- src/robertwray.co.uk/ResourceManifest.cs
+++ src/robertwray.co.uk/ResourceManifest.cs
@@ -12,19 +12,23 @@ namespace robertwraycouk
         {
             var manifest = builder.Add();
 
-            //manifest.DefineScript("Bootstrap").SetUrl("bootstrap-3.3.5/js/bootstrap.min.js", "bootstrap-3.3.5/js/bootstrap.js").SetVersion("3.3.4").SetDependencies("jQuery");
-            //manifest.DefineScript("HoverDropdown").SetUrl("hover-dropdown.js").SetDependencies("Bootstrap");
-            //manifest.DefineScript("Stapel-Modernizr").SetUrl("stapel/modernizr.custom.63321.js");
-            //manifest.DefineScript("Stapel").SetUrl("stapel/jquery.stapel.js").SetDependencies("jQuery", "Stapel-Modernizr");
-            //manifest.DefineScript("prettyPhoto").SetUrl("prettyPhoto/jquery.prettyPhoto.js").SetDependencies("jQuery");
-            //manifest.DefineScript("Custom").SetUrl("custom.js").SetDependencies("jQuery");
-
-            //manifest.DefineStyle("Stapel").SetUrl("stapel/stapel.css");
-            //manifest.DefineStyle("prettyPhoto").SetUrl("prettyPhoto/prettyPhoto.css");
-
             manifest.DefineScript("popper").SetUrl("https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js").SetVersion("1.16.0");
-            manifest.DefineScript("Bootstrap").SetUrl("https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js").SetVersion("3.3.4").SetDependencies("jQuery", "popper");
-            
+            manifest.DefineScript("Bootstrap")
+                .SetUrl("https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js")
+                .SetVersion("4.4.1")
+                .SetCdn("//stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js")
+                .SetDependencies("jQuery", "popper");
+            //manifest.DefineScript("Bootstrap").SetUrl("bootstrap-3.3.5/js/bootstrap.min.js", "bootstrap-3.3.5/js/bootstrap.js").SetVersion("3.3.4").SetDependencies("jQuery");
+            manifest.DefineScript("HoverDropdown").SetUrl("hover-dropdown.js").SetDependencies("Bootstrap");
+            manifest.DefineScript("Stapel-Modernizr").SetUrl("stapel/modernizr.custom.63321.js");
+            manifest.DefineScript("Stapel").SetUrl("stapel/jquery.stapel.js").SetDependencies("jQuery", "Stapel-Modernizr");
+            manifest.DefineScript("prettyPhoto").SetUrl("prettyPhoto/jquery.prettyPhoto.js").SetDependencies("jQuery");
+            manifest.DefineScript("Custom").SetUrl("custom.js").SetDependencies("jQuery");
+
+            manifest.DefineStyle("Stapel").SetUrl("stapel/stapel.css");
+            manifest.DefineStyle("prettyPhoto").SetUrl("prettyPhoto/prettyPhoto.css");
+
+
 
             manifest.DefineScript("CarouselHelper").SetUrl("carousel-helper.js").SetVersion("1.0.0").SetDependencies("jQuery", "Bootstrap");
         }
