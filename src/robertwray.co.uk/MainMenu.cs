cdb7fe485b42195721f94f6190a20538017eebea
Robert Wray <2657405+robertwray@users.noreply.github.com>
2021-03-01 07:54:01 +0000

    Pre-Footer implementation

--- src/robertwray.co.uk/MainMenu.cs
+++ src/robertwray.co.uk/MainMenu.cs
@@ -7,19 +7,24 @@ using Orchard.Localization;
 using Orchard.UI.Navigation;
 using Orchard.Core.Title.Models;
 
-namespace PJS.Bootstrap {
-    public class MainMenu : IMenuProvider {
+namespace PJS.Bootstrap
+{
+    public class MainMenu : IMenuProvider
+    {
         private readonly IContentManager _contentManager;
         private readonly IOrchardServices _orchardServices;
 
-        public MainMenu(IContentManager contentManager, IOrchardServices orchardServices) {
+        public MainMenu(IContentManager contentManager, IOrchardServices orchardServices)
+        {
             _contentManager = contentManager;
             _orchardServices = orchardServices;
         }
 
         public Localizer T { get; set; }
-        public void GetMenu(IContent menu, NavigationBuilder builder) {
-            if (menu.As<TitlePart>().Title == "Main Menu") {
+        public void GetMenu(IContent menu, NavigationBuilder builder)
+        {
+            if (menu.As<TitlePart>().Title == "Main Menu")
+            {
                 var maxPosition = _contentManager
                     .Query<MenuPart, MenuPartRecord>()
                     .Where(x => x.MenuId == menu.Id)
@@ -29,15 +34,18 @@ namespace PJS.Bootstrap {
 
                 var itemCount = maxPosition + 1;
 
-                if (_orchardServices.WorkContext.CurrentUser != null) {
+                if (_orchardServices.WorkContext.CurrentUser != null)
+                {
                     builder.Add(T(_orchardServices.WorkContext.CurrentUser.UserName), itemCount.ToString(), item => item.Url("#").AddClass("menuUserName"));
                     builder.Add(T("Change Password"), itemCount.ToString() + ".1", item => item.Action("ChangePassword", "Account", new { area = "Orchard.Users" }));
                     builder.Add(T("Sign Out"), itemCount.ToString() + ".2", item => item.Action("LogOff", "Account", new { area = "Orchard.Users", ReturnUrl = _orchardServices.WorkContext.HttpContext.Request.RawUrl }));
-                    if (_orchardServices.Authorizer.Authorize(Orchard.Security.StandardPermissions.AccessAdminPanel)) {
+                    if (_orchardServices.Authorizer.Authorize(Orchard.Security.StandardPermissions.AccessAdminPanel))
+                    {
                         builder.Add(T("Dashboard"), itemCount.ToString() + ".3", item => item.Action("Index", "Admin", new { area = "Dashboard" }));
                     }
                 }
-                else {
+                else
+                {
                     builder.Add(T("Sign In"), itemCount.ToString(), item => item.Action("LogOn", "Account", new { area = "Orchard.Users", ReturnUrl = (_orchardServices.WorkContext.HttpContext.Request.QueryString["ReturnUrl"] ?? _orchardServices.WorkContext.HttpContext.Request.RawUrl) }));
                 }
             }
